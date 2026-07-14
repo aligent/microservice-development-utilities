14af09e50cc5a7cc403cae553ffb3141e82c330a
Kai Nguyen <kai.nguyen@aligent.com.au>
2026-07-14 11:13:13 +1000

    MI-331: Export ClientOptions for throwing client

--- packages/microservice-util-lib/src/openapi-fetch-middlewares/throwing-client.ts
+++ packages/microservice-util-lib/src/openapi-fetch-middlewares/throwing-client.ts
@@ -22,6 +22,7 @@
  */
 
 import type { ClientOptions, MaybeOptionalInit, Middleware, ParseAsResponse } from 'openapi-fetch';
+export type { ClientOptions } from 'openapi-fetch';
 import createClient from 'openapi-fetch';
 import type {
     HttpMethod,
